57cdfba40ddd4ce81223b9f4a563cb503bfdcb43
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-05-03 19:37:04 +0200

    Add new array extension for dupliactes

---
+++ Essentials/Sources/Essentials/Array+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cédric Bahirwe on 03/05/2023.
+//
+
+import Foundation
+
+extension Array where Element: Hashable {
+    func removingDuplicates() -> [Element] {
+        var addedDict = [Element: Bool]()
+
+        return filter {
+            addedDict.updateValue(true, forKey: $0) == nil
+        }
+    }
+
+    mutating func removeDuplicates() {
+        self = self.removingDuplicates()
+    }
+}
+
